65450caf22727fcdad26ed8a307fe638ea768d53
木杉 <zhmushan@qq.com>
2020-04-19 21:02:43 +0800

    修复 Edge 加载速度过慢的问题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import Eplayer from "eplayer";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    customElements.define("e-player", Eplayer);
+  }
+}
